d878282e02d736354dc5a72f43ecd32795c242a1
lemon <1406428663@qq.com>
2019-02-25 17:20:58 +0800

    Update mutations.js

--- src/store/mutations.js
+++ src/store/mutations.js
@@ -1,11 +1,21 @@
 import {
+	GET_HOME,
+	GET_LIST,
 	ADD_ITEM,
 	DEL_ITEM,
 	CLEAR_CHECKED,
 	CHECK_ALL
 } from './mutation-types';
 
+import {getHome} from '../api'
+
 export default {
+	[GET_HOME](state) {
+		getHome().then(home => state.home = home);
+	},
+	[GET_LIST](state, {list}) {
+		state.list = list;
+	},
 	[ADD_ITEM](state, {text, log}) {
 		state.data.unshift({ text, finish: false });
 		console.log('this is log......' + log);
