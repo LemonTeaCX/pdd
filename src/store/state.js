01949f49c81ba988591941a67a128d4ca2cbf101
lemon <1406428663@qq.com>
2019-02-25 15:08:07 +0800

    Create state.js

---
+++ src/store/state.js
@@ -0,0 +1,7 @@
+export default {
+	data: [
+    { text: 'this is no.1 data', finish: false },
+    { text: 'this is no.2 data', finish: true },
+    { text: 'this is no.3 data', finish: false }
+  ]
+}
