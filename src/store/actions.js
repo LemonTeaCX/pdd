f65a2ce57d769ebcaeeffed0aff8321aa91b4cdb
lemon <1406428663@qq.com>
2019-02-25 17:20:30 +0800

    Update actions.js

--- src/store/actions.js
+++ src/store/actions.js
@@ -1,11 +1,25 @@
 import {
+	GET_HOME,
+	GET_LIST,
 	ADD_ITEM,
 	DEL_ITEM,
 	CLEAR_CHECKED,
 	CHECK_ALL
 } from './mutation-types';
 
+import {
+	getHome,
+	getList
+} from '../api'
+
 export default {
+	getHome({commit}) {
+		commit(GET_HOME);
+	},
+	async getList({commit}) {
+		const result = await getList();
+		commit(GET_LIST, {list: result.data});
+	},
 	addItem({commit}, text, log) {
 		commit(ADD_ITEM, {text, log});
 	},
