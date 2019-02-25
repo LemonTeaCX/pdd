574622acdb8c990c0829f97f1c39fca830e5a992
lemon <1406428663@qq.com>
2019-02-25 17:21:20 +0800

    Update mutation-types.js

--- src/store/mutation-types.js
+++ src/store/mutation-types.js
@@ -1,3 +1,5 @@
+export const GET_HOME = 'get_home';
+export const GET_LIST = 'get_list';
 export const ADD_ITEM = 'add_item';
 export const DEL_ITEM = 'del_item';
 export const CLEAR_CHECKED = 'clear_checked';
